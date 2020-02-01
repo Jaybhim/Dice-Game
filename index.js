9daa622bc0f9bf403c996ef4888bc2ff63d068d0
Jaybhim <jaybhim049@gmail.com>
2020-02-01 21:35:56 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var randomNumber1 = Math.floor(parseFloat(Math.random()*6)) + 1; // 1-6
+
+// var a = "dice";
+// var b = ".png";
+// var c = a.concat(randomNumber1);
+
+var diceRandomImage = "dice" + randomNumber1 + ".png"
+
+var randomImageSource = "images/" + diceRandomImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(parseFloat(Math.random()*6)) + 1;
+var diceRandomImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + diceRandomImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = " 🎉 Player 1 wins."
+}
+else if(randomNumber1 < randomNumber2)
+{
+  document.querySelector("h1").innerHTML = " 🎉 Player 2 wins."
+}
+else {
+  document.querySelector("h1").innerHTML = " Draw."
+}
